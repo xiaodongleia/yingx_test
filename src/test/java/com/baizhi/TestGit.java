7b698f2caa9f1adf41e3ad47640ce9f8707639db
xiaodongleia <63596699+xiaodongleia@users.noreply.github.com>
2020-04-14 23:11:38 +0800

    git

--- src/test/java/com/baizhi/TestGit.java
+++ src/test/java/com/baizhi/TestGit.java
@@ -2,6 +2,8 @@ package com.baizhi;
 
 public class TestGit {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
         System.out.println("==");
         System.out.println("dev");
     }
